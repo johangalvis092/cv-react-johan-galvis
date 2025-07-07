5a9efef26afdc59f180472555c1b598cce3b19f6
johangalvis092 <johangalvis102@gmail.com>
2025-07-07 20:04:15 +0000

    feat: componente Perfil dinámico con props

--- mi-app-react/src/components/Perfil.jsx
+++ mi-app-react/src/components/Perfil.jsx
@@ -1,10 +1,20 @@
-export default function Perfil() {
+export default function Perfil({texto}) {
   return (
     <section>
-      <h3>Perfil</h3>
+      <h2>Perfil Profesional</h2>
       <p>
-        Tecnologo en Analisis y Desarrollo de software con experiencia en desarrollo web y móvil, apasionado por la tecnología.
+        Soy un desarrollador Front-End en proceso de formación,
+         motivado por la creación de aplicaciones web interactivas y adaptables a distintos dispositivos. 
+         Me entusiasma aprender de forma continua y trabajar en equipo para encontrar soluciones a desafíos tanto técnicos como creativos.
       </p>
+      <h3>Habilidades Destacadas:</h3>
+      <ul>
+        <li>HTML, CSS, JavaScript</li>
+        <li>React.js y Vite</li>
+        <li>Trabajo en equipo</li>
+        <li>Comunicación efectiva</li>
+        <li>Aprendizaje autónomo</li>
+      </ul>
     </section>
   );
-}
+};
